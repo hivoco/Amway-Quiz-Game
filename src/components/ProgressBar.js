45042921cb081e67e78dfb0f804af64952bd3eda
ranjanhivoco <ranjan@hivoco.com>
2025-05-17 16:52:55 +0530

    progress bar fixed

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,17 +1,10 @@
-import { useState } from "react";
-
-const ProgressBar = ({count,animation}) => {
-  // const [currentStep, setCurrentStep] = useState(0);
+const ProgressBar = ({ count, animation }) => {
+  console.log(count);
+  
   const totalSteps = 10; // (10 dots total)
-
-  // const handleClick = () => {
-  //   // cap at totalSteps-1
-  //   setCurrentStep((prev) => (prev < totalSteps - 1 ? prev + 1 : prev));
-  // };
 
   return (
     <div
-      // onClick={handleClick}
       className={`flex w-full items-center justify-between relative py-1.5 mb-5
       transition-all duration-700 ease-in-out ${
         animation ? "translate-x-0 blur-none" : "-translate-y-15 blur-xs"
@@ -19,14 +12,20 @@ const ProgressBar = ({count,animation}) => {
         `}
     >
       {/* Line connecting all dots at the back-ground */}
-      <div className="absolute h-1 bg-[#BFBFBF] left-0 right-0 top-1/2 transform -translate-y-1/2 z-0"></div>
+
+      <div className="absolute h-1 bg-[#BFBFBF] left-0 right-0 top-1/2 transform -translate-y-1/2 z-0">
+        <div
+          style={{ width: `${count <= totalSteps && (count - 1) * 11}%` }}
+          className="!bg-[#00A55C] h-full"
+        />
+      </div>
 
       {/* Dots 10 */}
       {Array.from({ length: totalSteps }).map((_, index) => (
         <div
           key={index}
           className={`w-3 h-3 rounded-full z-10 transition-all duration-300 ease-out flex items-center justify-center
-                ${index <= count ? "bg-[#00A55C]" : "bg-[#BFBFBF]"}`}
+                ${index + 1 <= count ? "bg-[#00A55C]" : "bg-[#BFBFBF]"}`}
         />
       ))}
     </div>
